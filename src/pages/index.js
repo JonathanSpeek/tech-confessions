b227135e56afc651f7ecd87b117c51559210ff80
Jonathan Speek <jonspeek@gmail.com>
2018-07-31 09:55:10 -0600

    Added infinite scroll loading to CardList

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,95 @@ const CardList = styled.div`
   justify-items: center;
 `
 
-const IndexPage = (data) => (
-  <div>
-    <Header />
-    <SubmitForm />
-    <CardList>{data.data.allContentfulCard.edges.map(edge => (
-      <Card data={edge.node.text} date={edge.node.date} />
-    ))}
-    </CardList>
-  </div>
-)
+const LoadMore = styled.button`
+  font-weight: 600;
+  color: #3E30E0;
+  text-transform: uppercase;
+  background: #FFF;
+  padding: 18px 32px;
+  border-radius: 32px;
+  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
+  transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+  outline: none;
+  border: none;
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.1, 1.1);
+    box-shadow: 0 30px 60px rgba(0,0,0,0.5);
+  }
+`
+
+if (typeof window !== `undefined`) {
+  window.cardsToShow = 10
+}
+
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    let cardsToShow = 10
+    if (typeof window !== `undefined`) {
+      cardsToShow = window.cardsToShow
+    }
+
+    this.state = {
+      showingMore: cardsToShow > 10,
+      cardsToShow,
+    }
+  }
+
+  update() {
+    const distanceToBottom =
+      document.documentElement.offsetHeight -
+      (window.scrollY + window.innerHeight)
+    if (this.state.showingMore && distanceToBottom < 100) {
+      this.setState({ cardsToShow: this.state.cardsToShow + 10 })
+    }
+    this.ticking = false
+  }
+
+  handleScroll = () => {
+    if (!this.ticking) {
+      this.ticking = true
+      requestAnimationFrame(() => this.update())
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener(`scroll`, this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(`scroll`, this.handleScroll)
+    window.cardsToShow = this.state.cardsToShow
+  }
+  
+  render() {
+    const cards = this.props.data.allContentfulCard.edges.map(e => e.node)
+
+    return(
+      <div>
+        <Header />
+        <SubmitForm />
+        <CardList>
+          {cards.slice(0, this.state.cardsToShow).map(card => (
+            <Card data={card.text} date={card.date} />
+          ))}
+          {!this.state.showingMore && (
+          <LoadMore
+            onClick={() => {
+              this.setState({
+                postsToShow: this.state.postsToShow + 10,
+                showingMore: true,
+              })
+            }}
+          >
+            Load More
+          </LoadMore>
+          )}
+        </CardList>
+      </div>
+    )
+  }
+}
 
 export default IndexPage
